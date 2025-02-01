60dd3ecd19cb91ddc9bb3d511e8e05f899cd29ad
David de la Torre Abellán <159726858+D3iVeX@users.noreply.github.com>
2025-02-01 20:25:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from routers import users, products, jwt_auth_users, basic_auth_users, users_db
 from fastapi.staticfiles import StaticFiles
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
 
 
 app = FastAPI()
